65532c0b28943a8626361ce6621bfa55c5ea3294
HRP2 <HRP2@jsk.t.u-tokyo.jp>
2016-09-08 20:19:10 +0900

    add dummy file

---
+++ src/second_grasp_finder.cpp
@@ -0,0 +1,13 @@
+// subscribe grasp pose
+
+// subscribe depth before
+
+// load movement file
+
+// get before position(3D)
+
+// find point before
+
+// find near point
+
+// publish grasp pose in moved points
