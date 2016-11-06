812714daa8a0924fe9820072ad329a57031b17a6
YuOhara <ohara@jsk.imi.i.u-tokyo.ac.jp>
2016-11-06 19:21:24 +0900

    test for recoverpose

--- src/second_grasp_finder.cpp
+++ src/second_grasp_finder.cpp
@@ -18,6 +18,7 @@
 #include <math.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
 #include <openrave_test/RaveGraspArray.h>
 #include <rospack/rospack.h>
 /**
@@ -47,7 +48,16 @@ ros::Publisher *second_grasp_array_pub_;
 ros::Publisher *second_grasp_array_debug_pub_;
 ros::Publisher *second_rave_grasp_array_pub_;
 cv::Mat debug_img = cv::Mat::zeros(500, 500, CV_8UC3);
-
+cv::Scalar colors[8] =
+  {cv::Scalar(0, 0, 0),
+   cv::Scalar(0, 255, 0),
+   cv::Scalar(0, 0, 255),
+   cv::Scalar(255, 0, 0),
+   cv::Scalar(255, 0, 255),
+   cv::Scalar(255, 255, 0),
+   cv::Scalar(0, 255, 255),
+   cv::Scalar(255, 255, 255)
+  };
 void readTracks(std::vector<CSimpleTrack> &mTracks, std::string file_name=std::string("/moseg/TrainingSet/Results/OchsBroxMalik4_all_0000020.00/fromrobot/Tracks20.dat")) {
   // open a file
 
@@ -126,7 +136,7 @@ CPoint getBefore(CPoint after_point, std::vector<CSimpleTrack> &mTracksGrasp) {
   return return_point;
 }
 
-int getLabel(double x, double y, std::vector<CSimpleTrack> &mTracks, std::vector<CSimpleTrack> &mTracksGrasp, cv::Mat &H)
+int getLabel(double x, double y, std::vector<CSimpleTrack> &mTracks, std::vector<CSimpleTrack> &mTracksGrasp, Eigen::Affine3d &output)
 {
   int label = -1;
   int store_size = 8;
@@ -151,8 +161,8 @@ int getLabel(double x, double y, std::vector<CSimpleTrack> &mTracks, std::vector
         mins.insert(it, dist);
         points.insert(point_it, before_point);
         points_after.insert(point_after_it, 
-                            before_point
-                            //mTracks[j].mPoints.back()
+                            // before_point
+                            mTracks[j].mPoints.back()
                             );
         mins.pop_back();
         points.pop_back();
@@ -177,14 +187,40 @@ int getLabel(double x, double y, std::vector<CSimpleTrack> &mTracks, std::vector
   for (int i=0; i<store_size; i++) {
     before_points[i] = cv::Point2f(points[i].x, points[i].y);
     after_points[i] = cv::Point2f(points_after[i].x, points_after[i].y);
-    ROS_INFO("%f %f -> %f %f", points[i].x, points[i].y, points_after[i].x, points_after[i].y);
+    ROS_INFO("%f %f -> %f %f", before_points[i].x, before_points[i].y, after_points[i].x, after_points[i].y);
+    cv::line(debug_img, before_points[i], after_points[i], colors[i%8]);
   }
   // cv::Mat(before_points);
   cv::Mat before_points_mat (cv::Size(store_size, 1), CV_32FC2, before_points);
   cv::Mat after_points_mat (cv::Size(store_size, 1), CV_32FC2, after_points);
-  H = cv::findHomography(before_points_mat, after_points_mat, CV_RANSAC, 3 /* reprojection thre*/);
-  ROS_INFO("hige %f %f %f", H.at<double>(0, 0),  H.at<double>(0, 1),  H.at<double>(0, 2));
+  // 焦点距離とレンズ主点
+  double fovx, fovy, focal, pasp;
+  cv::Point2d pp;
+  cv::calibrationMatrixValues(model_.intrinsicMatrix(), cv::Size(cam_info_->width, cam_info_->height), 0.0, 0.0, fovx, fovy, focal, pp, pasp);
+  cv::Matx33d R;
+  cv::Matx31d T;
+  cv::Mat E = cv::findEssentialMat(before_points_mat, after_points_mat, focal, pp);
+  std::cout << "Essential" << std::endl;
+  std::cout << E << std::endl;
+  cv::recoverPose(E, before_points_mat, after_points_mat, R, T, focal, pp);
+  std::cout << "R" << std::endl;
+  std::cout << R << std::endl;
+  std::cout << "T" << std::endl;
+  std::cout << T << std::endl;
 
+  tf::Transform resulttf;
+  resulttf.setOrigin( tf::Vector3(T(0, 0), T(1, 0), T(2, 0)));
+  tf::Matrix3x3 mat;
+  mat.setValue(R(0, 0), R(0, 1), R(0, 2),
+               R(1, 0), R(1, 1), R(1, 2),
+               R(2, 0), R(2, 1), R(2, 2));
+  resulttf.setBasis(mat);
+  tf::transformTFToEigen(resulttf, output);
+  ROS_INFO("hige %f %f %f", T(0, 0), T(1, 0), T(2, 0));  
+  ROS_INFO("huge %f %f %f, %f %f %f, %f %f %f", R(0, 0), R(0, 1), R(0, 2),
+           R(1, 0), R(1, 1), R(1, 2),
+           R(2, 0), R(2, 1), R(2, 2)
+           );
   std::stringstream ss;
   ss << " label: " << label;
   std::string output_txt = ss.str();
@@ -193,11 +229,11 @@ int getLabel(double x, double y, std::vector<CSimpleTrack> &mTracks, std::vector
   return label;
 }
 
-int getLabel(Eigen::Vector3d grasp_cam, std::vector<CSimpleTrack> &mTracks, std::vector<CSimpleTrack> &mTracksGrasp, cv::Mat &H)
+int getLabel(Eigen::Vector3d grasp_cam, std::vector<CSimpleTrack> &mTracks, std::vector<CSimpleTrack> &mTracksGrasp, Eigen::Affine3d &output)
 {
     cv::Point3d p(grasp_cam.x(), grasp_cam.y(), grasp_cam.z());
     cv::Point2d uv = model_.project3dToPixel(p);
-    return getLabel(uv.x, uv.y, mTracks, mTracksGrasp, H);
+    return getLabel(uv.x, uv.y, mTracks, mTracksGrasp, output);
 }
 
 void graspPoseCallback(const openrave_test::RaveGraspArrayConstPtr& rave_grasp, const geometry_msgs::PoseArrayConstPtr& com)
@@ -232,77 +268,15 @@ void cameraInfoCallback(const sensor_msgs::CameraInfoConstPtr& info)
       ROS_INFO("failed to create camera model");
       return;
     }
+    double fovx, fovy, focal, pasp;
+    cv::Point2d pp;
+    cv::calibrationMatrixValues(model_.intrinsicMatrix(), cv::Size(cam_info_->width, cam_info_->height), 0.0, 0.0, fovx, fovy, focal, pp, pasp);
+    std::cout<< "focal: " << focal << std::endl;
   }
 }
 
-Eigen::Affine3d transFromHomography(cv::Mat H)
+Eigen::Affine3d transFromAffine(cv::Mat H)
 {
-  // cv::Mat pose = cv::Mat::eye(3, 4, CV_64FC1);      // 3x4 matrix, the camera pose
-  // float norm1 = (float)norm(H.col(0));  
-  // float norm2 = (float)norm(H.col(1));  
-  // float tnorm = (norm1 + norm2) / 2.0f; // Normalization value
-  // cv::Mat p1 = H.col(0);       // Pointer to first column of H
-  // cv::Mat p2 = pose.col(0);    // Pointer to first column of pose (empty)
-  // cv::normalize(p1, p2);   // Normalize the rotation, and copies the column to pose
-  // ROS_INFO("hage %f %f %f", H.col(0).at<double>(0),  H.col(0).at<double>(1),  H.col(0).at<double>(2));
-  // p1 = H.col(1);           // Pointer to second column of H
-  // p2 = pose.col(1);        // Pointer to second column of pose (empty)
-  // cv::normalize(p1, p2);   // Normalize the rotation and copies the column to pose
-  // p1 = pose.col(0);
-  // p2 = pose.col(1);
-  // cv::Mat p3 = p1.cross(p2);   // Computes the cross-product of p1 and p2
-  // cv::Mat c2 = pose.col(2);    // Pointer to third column of pose
-  // p3.copyTo(c2);       // Third column is the crossproduct of columns one and two
-  // pose.col(3) = H.col(2) / tnorm;  //vector t [R|t] is the last column of pose
-  // tf::Transform checktf;
-  // ROS_INFO("piyo %f %f %f", pose.at<double>(0, 3), pose.at<double>(1, 3), pose.at<double>(2, 3));
-  // checktf.setOrigin( tf::Vector3(pose.at<double>(0, 3), pose.at<double>(1, 3), pose.at<double>(2, 3)) );
-  // tf::Matrix3x3 rotation_mat(pose.at<double>(0, 0), pose.at<double>(0, 1), pose.at<double>(0, 2),
-  //                            pose.at<double>(1, 0), pose.at<double>(1, 1), pose.at<double>(1, 2),
-  //                            pose.at<double>(2, 0), pose.at<double>(2, 1), pose.at<double>(2, 2));
-  // checktf.setBasis(rotation_mat);
-  // Eigen::Affine3d transform;
-  // tf::transformTFToEigen(checktf, transform);
-  // return transform;
-
-  cv::Point2f corners2d[4] = {cv::Point2f(0,0),
-                              cv::Point2f(100,0),
-                              cv::Point2f(100,100),
-                              cv::Point2f(0,100)};
-  cv::Mat corners2d_mat (cv::Size(4, 1), CV_32FC2, corners2d);
-  cv::Point3f corners3d[4] = {cv::Point3f(0,0,0),
-                              cv::Point3f(0,0.1,0),
-                              cv::Point3f(0.1,0.1,0),
-                              cv::Point3f(0.1,0,0)};
-  cv::Mat corners3d_mat (cv::Size(4, 1), CV_32FC3, corners3d);
-  cv::Mat corners2d_mat_trans;
-  cv::perspectiveTransform (corners2d_mat, corners2d_mat_trans, H);
-  corners2d_mat.at<cv::Point2f>();
-  ROS_INFO("translated corner (%f %f) (%f %f)...",
-           corners2d_mat_trans.at<cv::Point2f>(0).x,
-           corners2d_mat_trans.at<cv::Point2f>(0).y,
-           corners2d_mat_trans.at<cv::Point2f>(1).x,
-           corners2d_mat_trans.at<cv::Point2f>(1).y
-           );
-  double fR3[3], fT3[3];
-  cv::Mat rvec(3, 1, CV_64FC1, fR3);
-  cv::Mat tvec(3, 1, CV_64FC1, fT3);
-  cv::Mat zero_distortion_mat = cv::Mat::zeros(4, 1, CV_64FC1);
-  cv::solvePnP (corners3d_mat, corners2d_mat_trans, 
-                model_.intrinsicMatrix(),
-                zero_distortion_mat,//if unrectified: pcam.distortionCoeffs()
-                rvec, tvec);
-
-  tf::Transform checktf;
-  checktf.setOrigin( tf::Vector3(fT3[0], fT3[1], fT3[2] ) );
-  double rx = fR3[0], ry = fR3[1], rz = fR3[2];
-  tf::Quaternion quat;
-  double angle = cv::norm(rvec);
-  quat.setRotation(tf::Vector3(rx/angle, ry/angle, rz/angle), angle);
-  checktf.setRotation( quat );
-  Eigen::Affine3d transform;
-  tf::transformTFToEigen(checktf, transform);
-  return transform;
 }
 
 
@@ -328,7 +302,7 @@ bool loadMovementFile(openrave_test::SecondGrasp::Request  &req,
   // project all grasps to movement
 
   std::vector<CSimpleTrack> nullTracks;
-  cv::Mat hand_trans; // not needed
+  Eigen::Affine3d hand_trans; // not needed
   int hand_label = getLabel(getTransform(cam_info_->header.frame_id, "rarm_end_coords").translation() ,mTracks, nullTracks, hand_trans);
   ROS_INFO("label! %d", hand_label);
   int back_label = 0; // may be 0
@@ -345,8 +319,8 @@ bool loadMovementFile(openrave_test::SecondGrasp::Request  &req,
   for (int i = 0; i < com_array_odom.size(); i++) {
     Eigen::Vector3d grasp_cam;
     grasp_cam = transform * com_array_odom[i];
-    cv::Mat H;
-    int label = getLabel(grasp_cam, mTracks, mTracksGrasp, H);
+    Eigen::Affine3d output;
+    int label = getLabel(grasp_cam, mTracks, mTracksGrasp, output);
     ROS_INFO("label%d:, %d", i, label);
     omp_set_lock(&writelock);
     // one thread at a time stuff
@@ -354,17 +328,21 @@ bool loadMovementFile(openrave_test::SecondGrasp::Request  &req,
       ROS_INFO("succeeded");
       // push back grasp pose
       // H to Eigen
-      Eigen::Affine3d local_trans = transFromHomography(H);
+      Eigen::Affine3d offset = Eigen::Affine3d(Eigen::AngleAxisd(
+                                                                 M_PI
+                                                                 , Eigen::Vector3d(1, 0, 0)));
+      Eigen::Affine3d local_trans = (offset * output);
       ROS_INFO("fuga %f %f %f", local_trans.translation().x(), local_trans.translation().y(), local_trans.translation().z());
       geometry_msgs::Pose grasp_pose;
-      tf::poseEigenToMsg(transform.inverse()* local_trans * transform *
-                         grasp_array_odom[i], grasp_pose);
+      tf::poseEigenToMsg(transform.inverse()  * local_trans * (transform * grasp_array_odom[i])
+                         , grasp_pose);
       second_grasp_pose_array.poses.push_back(grasp_pose);
       finger_angle_array_out.push_back(finger_angle_array_[i]);
       geometry_msgs::Pose grasp_pose2;
       tf::poseEigenToMsg(
                          grasp_array_odom[i], grasp_pose2);
       second_grasp_pose_array_debug.poses.push_back(grasp_pose2);
+      // break;
     }
     else {
       geometry_msgs::Pose grasp_pose;
