7658e70636b557394f60c3cf2eefee25b30de33c
YuOhara <ohara@jsk.imi.i.u-tokyo.ac.jp>
2017-01-14 16:26:14 +0900

    change re_do

--- scripts/re_do_calc.sh
+++ scripts/re_do_calc.sh
@@ -2,5 +2,6 @@ for file in `\find ~/Result/ -maxdepth 1 -mindepth 1 -type d`; do
     # TODO
     echo $file
     cp $file/* ~/.ros/ -r
-    rosrun openrave_test grasp_finder_with_load.py > $file/result.txt
+    rosrun openrave_test grasp_finder_with_load.py > $file/result4.txt
+    cp -r ~/.ros/grasps/ $file/grasps4
 done
